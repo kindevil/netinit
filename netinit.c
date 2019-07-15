a03a8097cf9a4a0f83551451caac14ea9670049d
kindevil <kindevil@outlook.com>
2019-07-15 17:29:30 +0800

    update

--- netinit.c
+++ netinit.c
@@ -1,23 +1,13 @@
 #include <netinit.h>
 
-void net_config()
+void net_config(char *address, char *netmask, char *gateway)
 {
     ip_addr_t addr, mask, gtway;
     struct netdev *netdev = RT_NULL;
 
-    cJSON* address = cJSON_GetObjectItem(network, "address");
-    cJSON* netmask = cJSON_GetObjectItem(network, "netmask");
-    cJSON* gateway = cJSON_GetObjectItem(network, "gateway");
-
-    rt_kprintf("###########################\n");
-    rt_kprintf("# address: %s \n", address->valuestring);
-    rt_kprintf("# netmask: %s \n", netmask->valuestring);
-    rt_kprintf("# gateway: %s \n", gateway->valuestring);
-    rt_kprintf("###########################\n");
-
-    inet_aton(address->valuestring, &addr);
-    inet_aton(netmask->valuestring, &mask);
-    inet_aton(gateway->valuestring, &gtway);
+    inet_aton(address, &addr);
+    inet_aton(netmask, &mask);
+    inet_aton(gateway, &gtway);
 
     /* 通过名称获取网卡对象 */
     netdev = netdev_get_by_name("e0");
--- netinit.h
+++ netinit.h
@@ -3,8 +3,7 @@
 
 #include <arpa/inet.h>
 #include <netdev.h>
-#include <cJSON.h>
 
-int init_network(void);
+void net_config(char *address, char *netmask, char *gateway);
 
 #endif /* __OPTPARSE_H_ */
